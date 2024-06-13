a55658428dc901d4569229531f9bbba4253eb295
Staphone Omondi <Staphone@SLO.COM>
2024-06-13 16:25:00 +0300

    Simple graphql fetching logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,38 @@
 import { useState } from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const client = new ApolloClient({
+    uri: "http://localhost:4000/",
+    cache: new InMemoryCache(),
+  });
+
+  client
+    .query({
+      query: gql`
+        query Book {
+          books {
+            title
+            author
+            coverPhotoURL
+            readingLevel
+          }
+        }
+      `,
+    })
+    .then((result) => console.log(result));
+
   return (
-    <>
-      <p className="text-lg font-bold ">Hello</p>
-    </>
+    <div className="">
+      <p className="text-lg font-bold font-mulish">Hello</p>
+    </div>
   );
 }
 
