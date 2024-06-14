ccc49f508c9c044553ab1ad1543452e3a7b7a4b0
Staphone Omondi <Staphone@SLO.COM>
2024-06-14 08:17:23 +0300

    converted to an arrow function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,11 @@ import { ApolloProvider } from "@apollo/client";
 import Landing from "./Landing";
 import client from "./api/client";
 
-function App() {
+const App = () => (
   <ApolloProvider client={client}>
     {/* This is the landing page for the app */}
     <Landing />
-  </ApolloProvider>;
-}
+  </ApolloProvider>
+);
 
 export default App;
