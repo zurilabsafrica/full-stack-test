362e7528882a713b1952b9d126c4e63ef4141e78
Staphone Omondi <Staphone@SLO.COM>
2024-06-14 14:17:47 +0300

    handle open/close functions

--- frontend/src/Landing.jsx
+++ frontend/src/Landing.jsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@mui/material/Card";
 
 import useBooks from "./hooks/useBooks";
 import Navbar from "./components/Navbar";
 import BookCard from "./components/BookCard";
+import Modal from "./components/Modal";
+import { Box, Skeleton } from "@mui/material";
 
 const Landing = () => {
   const { loading, error, data } = useBooks();
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedBook, setSelectedBook] = useState(null);
 
-  if (loading) return <p>Loading...</p>;
+  const handleOpen = (book) => {
+    setSelectedBook(book);
+    setOpenModal(true);
+  };
+
+  const handleClose = () => {
+    setOpenModal(false);
+  };
+
+  if (loading)
+    return (
+      <Box sx={{ pt: 0.5 }}>
+        {/* <Skeleton /> */}
+        <Skeleton width="60%" variant="circular" />
+      </Box>
+    );
   if (error) return <p>Error: {error.message}</p>;
   return (
     <div>
-      <Navbar />
-      <div className="flex justify-center">
+      <Navbar books={data.books} />
+      <div className="flex justify-center overflow-auto">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mx-auto">
           {data.books.map((book, i) => (
             <Card
               key={i}
               className="shadow-md rounded-3xl w-64 h-64 cursor-pointer transform transition-transform duration-10 ease-in-out hover:scale-105"
+              onClick={() => handleOpen(book)}
             >
               <img
                 className="object-cover w-64 h-64 "
@@ -29,6 +49,7 @@ const Landing = () => {
           ))}
         </div>
       </div>
+      <Modal open={openModal} handleClose={handleClose} book={selectedBook} />
     </div>
   );
 };
