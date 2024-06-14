59d1bd9f5bd517ad06ced5ce6270b1a9d82a168d
Staphone Omondi <Staphone@SLO.COM>
2024-06-14 08:04:14 +0300

    Removed Apollo client wrapper

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ApolloProvider } from "@apollo/client";
 
 import App from "./App.jsx";
 import "./index.css";
-import client from "./api/client";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 );
