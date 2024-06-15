2171a3c3d2080c3ad974df6c78ff41b538c54442
Staphone <staphoneomondi@gmail.com>
2024-06-15 16:40:14 +0300

    Fixed scrolling styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,12 +5,11 @@ import Search from "./Search";
 
 const Navbar = ({ books }) => {
   return (
-    <div className="fixed top-0 left-0 right-0 z-10">
-      <header className="bg-white p-4 w-full sm:w-3/4 md:w-1/2 lg:w-1/3 mx-auto shadow-md border-3xl">
+    <div className="fixed top-0 left-0 right-0 z-10 bg-white bg-opacity-75 backdrop-blur-sm shadow-md border-b-2">
+      <header className="p-4 w-full sm:w-3/4 md:w-1/2 lg:w-1/3 mx-auto">
         <div className="w-full flex justify-center">
           <div className="relative w-full">
-            {/* Search component */}
-            <Search books={books}/>
+            <Search books={books} />
           </div>
         </div>
       </header>
