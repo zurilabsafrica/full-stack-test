8eef6eb11a508876b0c0d42d750752bc6d5b6b91
Staphone <staphoneomondi@gmail.com>
2024-06-15 16:41:30 +0300

    Loading skeleton styling

--- frontend/src/Landing.jsx
+++ frontend/src/Landing.jsx
@@ -21,11 +21,17 @@ const Landing = () => {
 
   if (loading)
     return (
-      <Box sx={{ pt: 0.5 }}>
-        {/* <Skeleton /> */}
-        {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
-          <Skeleton key={item} width="60%" variant="circular" />
-        ))}
+      <Box
+        sx={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: 9999,
+        }}
+      >
+        <Skeleton variant="rectangular" width="100%" height="100%" />
       </Box>
     );
   if (error) return <p>Error: {error.message}</p>;
