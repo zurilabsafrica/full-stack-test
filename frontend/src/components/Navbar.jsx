985d91c63798beda846fdec6800c6efd5c2c1499
Staphone <staphoneomondi@gmail.com>
2024-06-15 13:26:17 +0300

    Styling on autocomplete modified

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,28 +1,38 @@
 import React from "react";
 import { Search } from "@mui/icons-material";
-import { Autocomplete, TextField } from "@mui/material";
+import { Autocomplete } from "@mui/material";
 
 const Navbar = ({ books }) => {
   return (
-    <div className="fixed w-full flex justify-center bg-white z-10">
-      <header className=" bg-white p-4 sm:w-3/4 md:w-1/2 lg:w-1/3 fixed left-1/2 transform -translate-x-1/2 z-10 shadow-md border-2xl">
-        <div className="mx-auto flex justify-between items-center">
-          <div className="text-lg font-semibold">My Application</div>
-          <div className="relative">
+    <div className="fixed top-0 left-0 right-0 z-10">
+      <header className="bg-white p-4 w-full sm:w-3/4 md:w-1/2 lg:w-1/3 mx-auto shadow-md border-3xl">
+        <div className="w-full flex justify-center">
+          <div className="relative w-full">
             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-              <Search className="h-5 w-5 text-gray-400" />
+              <Search className="h-4 w-4 text-steelBlue" />
             </div>
             <Autocomplete
               freeSolo
               id="free-solo-2-demo"
               disableClearable
-              options={books.map((book) => book.title)}
+              options={books}
+              getOptionLabel={(option) => option.title}
+              renderOption={(props, option) => (
+                <li {...props} className="flex items-center my-2 mx-1">
+                  <img
+                    src={`/${option.coverPhotoURL}`}
+                    alt={option.title}
+                    className="h-10 w-10 mr-3 rounded-lg"
+                  />
+                  {option.title}
+                </li>
+              )}
               renderInput={(params) => (
-                <div ref={params.InputProps.ref}>
+                <div ref={params.InputProps.ref} className="w-full">
                   <input
                     {...params.inputProps}
-                    className="w-full pl-10 pr-4 py-2 rounded-lg text-sm ring-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-600"
-                    placeholder="Search…"
+                    className="w-full pl-10 pr-4 py-2 rounded-lg text-sm ring-turquoise focus:outline-none focus:ring-2 focus:ring-turquoise"
+                    placeholder="Search Here"
                     aria-label="search"
                   />
                 </div>
@@ -36,34 +46,3 @@ const Navbar = ({ books }) => {
 };
 
 export default Navbar;
-
-{
-  /* <input
-              className="pl-10 pr-4 py-2 rounded-lg text-sm ring-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-600"
-              placeholder="Search…"
-              aria-label="search"
-            /> */
-}
-
-{
-  /* <Autocomplete
-              freeSolo
-              id="free-solo-2-demo"
-              disableClearable
-              options={books.map((book) => book.title)}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Search input"
-                  InputLabelProps={{
-                    style: {}, // increase the font size of the label
-                  }}
-                  InputProps={{
-                    ...params.InputProps,
-                    type: "search",
-                  }}
-                  className="w-full pl-10 pr-4 py-2 rounded-lg text-sm ring-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-600"
-                />
-              )}
-            /> */
-}
