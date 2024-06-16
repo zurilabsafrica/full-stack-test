519eaa657f15eb4c5cc331b48fd9733c5f16b6d9
Staphone <staphoneomondi@gmail.com>
2024-06-16 13:30:59 +0300

    Used react-router-dom for navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,43 @@
-import { ApolloProvider } from "@apollo/client";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Landing from "./Landing";
-import client from "./api/client";
+import Landing from "./pages/Landing";
+import Listing from "./pages/Listing";
+import useBooks from "./hooks/useBooks";
+import { addAllBooks } from "./features/teacherSlice";
 
-const App = () => (
-  <ApolloProvider client={client}>
-    {/* This is the landing page for the app */}
-    <Landing />
-  </ApolloProvider>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [books, setBooks] = useState(null);
+
+  const handleFetch = () => {
+    const { load, error, data } = useBooks();
+    setLoading(load);
+    setError(error);
+    setBooks(data.books);
+    console.log("Fetched books from api:", books);
+  };
+
+  const handleDispatch = () => {
+    dispatch(addAllBooks(books));
+  };
+
+  // useEffect(() => {
+  //   handleFetch();
+  //   handleDispatch();
+  // }, []);
+
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/listing" element={<Listing />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
